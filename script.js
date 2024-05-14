c242c250c386a5924e25aaea2e409124d96bf581
Soumya Ranjan Mishra <118365110+smranjanmishra@users.noreply.github.com>
2024-05-14 15:18:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 function fibonacci(num) {
 // your code here
-	if (num === 0) return 0;
-    if (num === 1) return 1;
+	if (num === 0) 
+		return 0;
+    if (num === 1) 
+		return 1;
 	
 	let a = 0, b = 1, c;
 
 	for (let i = 2; i <= num; i++) {
-        next = a + b;
+        c = a + b;
         a = b;
-        b = next;
+        b = c;
     }
     return b;
 	
 }
-
 module.exports = fibonacci;
